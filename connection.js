282d1ff64ae4a8587a98b75fb468bc0e52524574
vino00619 <vinoth00619@gmail.com>
2022-09-21 23:42:54 +0530

    initial commit

---
+++ connection.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URL, ()=>{
+    console.log('connected to db')
+})
---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
+const User = require('./models/User');
+const Message = require('./models/Message')
+
+
+const rooms = ['general', 'Friends', 'Family', 'Office'];
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(cors());
+app.use('/users', userRoutes)
+require('./connection')
+
+const server = require('http').createServer(app);
+const PORT = process.env.PORT || 5001;
+const io = require('socket.io')(server,{
+    cors:{
+        origin : 'https://chat-app-main.netlify.app/',
+        methods : ['GET', 'POST']
+    }
+})
+async function getLastMessageFromRoom(room){
+    let roomMessages = await Message.aggregate([
+        {$match : {to : room}},
+        {$group : {_id : '$date', messagesByDate:{$push : '$$ROOT'}}}
+    ])
+    return roomMessages;
+}
+
+function sortRoomMessagesByDate(messages){
+    return messages.sort(function(a,b){
+        let date1 = a._id.split('/');
+        let date2 = b._id.split('/')
+        
+        date1 = date1[2] + date1[0] + date1[1];
+        date2 = date2[2] + date2[0] + date2[1];
+
+        return date1<date2 ? -1 : 1
+        
+    })
+}
+
+//socket connection
+
+io.on('connection', (socket)=> {
+
+socket.on('new-user', async() =>{
+    const members = await User.find();
+    io.emit('new-user', members)
+})
+
+socket.on('join-room', async(newRoom, previousRoom) => {
+    socket.join(newRoom);
+    socket.leave(previousRoom)
+    let roomMessages = await getLastMessageFromRoom(newRoom)
+    roomMessages = sortRoomMessagesByDate(roomMessages);
+    socket.emit('room-messages', roomMessages)
+})
+
+
+socket.on('message-room', async(room, content, time, date, sender) =>{
+
+const newMessage = await Message.create({content, from:sender, time, date, to:room });
+let roomMessages = await getLastMessageFromRoom(room);
+roomMessages = sortRoomMessagesByDate(roomMessages);
+//sending message to room
+io.to(room).emit('room-messages', roomMessages);
+socket.broadcast.emit('notifications', room)
+
+})
+
+
+app.delete('/logout', async(req, res)=>{
+    try {
+        const {_id, newMessages} = req.body;
+        const user = await User.findById(_id);
+        user.status = "Offline";
+        user.newMessages = newMessages;
+        await user.save();
+        const members = await User.find();
+        socket.broadcast.emit('new-user', members);
+        res.status(200).send();
+
+    } catch (e) {
+        console.log(e);
+        res.status(400).send();
+    }
+    
+})
+
+} )
+
+app.get('/', (req, res)=>{
+    res.json("Hello")
+})
+
+app.get('/rooms', (req, res)=>{
+    res.json(rooms)
+})
+
+server.listen(PORT, ()=> {
+console.log("listening, up and running", PORT)
+})
